f2b210220c24dfb35ff889996396499457f99d22
MigueMendz <androimigue@gmail.com>
2024-08-12 00:23:18 -0600

    [UPDATE] New Endpoint test in app

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Im migue version 1');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About me');
+});
+
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
